99d8077d3fa6145f90dd32cf32bcaeedeea0f691
RyanRen2023 <renxihai@gmail.com>
2024-05-21 20:22:17 -0400

    Finished Navigation Drawer".

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mobile Lab2 Home Page'),
     );
   }
 }
@@ -75,61 +61,49 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      drawer: Drawer(child: Text("Hi, there"),),
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          actions: [
+            OutlinedButton(onPressed: () {}, child: Text("Button1")),
+            OutlinedButton(onPressed: () {}, child: Text("Button2"))
+          ]),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                hintText: "Input Login Name",
-                border: OutlineInputBorder(),
-                labelText: "Login Name",
-              ),
-            ),
-            TextField(
-              controller: _passController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  hintText: "Input Password",
-                  border: OutlineInputBorder(),
-                  labelText: "Password"),
-            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text("Button 1")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text("Button 2")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text("Button 3")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text("Button 4")),
+            SizedBox(height: 20),
             ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    if (_passController.value.text == "QWERT123") {
+                    if (_passController.value.text == "QWERTY123") {
                       pngUrl = "images/a_light_bulb.png";
                     } else {
                       pngUrl = "images/stop_sign.png";
@@ -137,14 +111,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   });
                 },
                 child: const Text("Login")),
-            Image.asset(
-              pngUrl,
-              width: 200,
-              height: 200,
-            )
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.camera),label:'Camera'),
+        BottomNavigationBarItem(icon: Icon(Icons.add_call), label:'Phone'),
+      ],onTap: (btnIndex){
+        print(btnIndex)
+      },),
     );
   }
 }
