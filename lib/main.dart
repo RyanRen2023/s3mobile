bee513a4e7b3a18e27b026cb92295c0d9677180b
RyanRen2023 <renxihai@gmail.com>
2024-06-04 19:20:33 -0400

    Lab 3 final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -58,12 +60,19 @@ class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController _nameController;
   late TextEditingController _passController;
   String pngUrl = "images/question_mark.png";
+  String _message = "";
+  final String _keyName = "userName";
+  final String _keyPass = "userPass";
+  static var prefs = EncryptedSharedPreferences();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
     super.initState();
     _nameController = TextEditingController();
     _passController = TextEditingController();
+    // initDataFromSharedPrefs();
+    initDataFromEncryptSharedPrefs();
   }
 
   @override
@@ -71,6 +80,124 @@ class _MyHomePageState extends State<MyHomePage> {
     _nameController.dispose();
     _passController.dispose();
     super.dispose();
+  }
+
+  void showSaveDialogPrompt() {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text("Saving Prompt"),
+        content: const Text(
+            "Saving your Username and Password for next time using?"),
+        actions: <Widget>[
+          ElevatedButton(
+              onPressed: saveEncryptedSharedPreference,
+              child: const Text("Yes")),
+          ElevatedButton(
+              onPressed: clearDataInDialog,
+              child: const Text("No")),
+        ],
+      ),
+    );
+  }
+
+  void changeImages() {
+    setState(() {
+      if (_passController.value.text == "QWERT123") {
+        pngUrl = "images/a_light_bulb.png";
+      } else {
+        pngUrl = "images/stop_sign.png";
+      }
+    });
+  }
+
+
+
+  void initDataFromEncryptSharedPrefs() async {
+
+    String? name = await prefs.getString(_keyName);
+    String? pass = await prefs.getString(_keyPass);
+
+    if (isNotEmpty(name) && isNotEmpty(pass)) {
+      setState(() {
+        _nameController.text = name;
+        _passController.text = pass;
+      });
+      showSnackBarWithClearAction("Load User Data Successfully!");
+    }
+  }
+
+  void saveEncryptedSharedPreference() async {
+    var name = _nameController.value.text;
+    var pass = _passController.value.text;
+    await prefs.setString(_keyName, name);
+    await prefs.setString(_keyPass, pass);
+    hiddenDialog();
+    showSnackBar("Save successfully!");
+  }
+
+  void clearDataInDialog(){
+    hiddenDialog();
+    clearEncryptedSharedPreference();
+    showSnackBar("User data has been removed successfully!");
+  }
+
+  void clearDataInSnack(){
+    clearEncryptedSharedPreference();
+    showSnackBar("User data has been removed successfully!");
+  }
+
+
+  void clearEncryptedSharedPreference() async {
+
+    prefs.getString(_keyName).then((value) {
+      if (isNotEmpty(value)) {
+         prefs.remove(_keyName);
+      }
+    });
+
+
+    prefs.getString(_keyPass).then((value) {
+      if (isNotEmpty(value)) {
+         prefs.remove(_keyPass);
+      }
+    });
+
+    setState(() {
+      _nameController.text = "";
+      _passController.text = "";
+    });
+
+  }
+
+  // validate a string is not null or empty
+  bool isNotEmpty(String? value) {
+    if (value == null || value.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
+
+
+  void hiddenDialog() async{
+     Navigator.pop(context);
+  }
+
+  void showSnackBar(message) {
+    var snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showSnackBarWithClearAction(message) async {
+    var snackBar = SnackBar(
+        content: Text(message),
+        action: SnackBarAction(
+          label: "Clear",
+          onPressed: clearDataInSnack,
+        ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    return;
   }
 
   @override
@@ -118,6 +245,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 labelText: "Login Name",
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
             TextField(
               controller: _passController,
               obscureText: true,
@@ -126,22 +256,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   border: OutlineInputBorder(),
                   labelText: "Password"),
             ),
+            const SizedBox(
+              height: 10,
+            ),
             ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    if (_passController.value.text == "QWERT123") {
-                      pngUrl = "images/a_light_bulb.png";
-                    } else {
-                      pngUrl = "images/stop_sign.png";
-                    }
-                  });
+                  var name = _nameController.value.text;
+                  var pass = _passController.value.text;
+                  if (isNotEmpty(name) && isNotEmpty(pass)) {
+                    changeImages();
+                    showSaveDialogPrompt();
+                  } else {
+                    setState(() {
+                      _message = 'Login name or password should not be empty';
+                    });
+                    _focusNode.requestFocus();
+                  }
                 },
                 child: const Text("Login")),
-            Image.asset(
-              pngUrl,
-              width: 200,
-              height: 200,
-            )
+            const SizedBox(
+              height: 20,
+            ),
+            Text(_message),
+            // Image.asset(
+            //   pngUrl,
+            //   width: 200,
+            //   height: 200,
+            // )
           ],
         ),
       ),
